fac7ea91741423a06a4a41111c3632783c79d067
Jarmo Jokela <jarmo.jokela@datasnipper.com>
2024-10-11 14:47:38 +0200

    initial commit

---
+++ NamedPipedTest.Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.IO.Pipes;
+
+namespace NamedPipedTest.Client;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Healthcare client is running...");
+
+        Console.WriteLine("Enter the path of file to send:");
+        var filePath = Console.ReadLine();
+
+        if (!File.Exists(filePath))
+        {
+            Console.WriteLine("File not found :(");
+            return;
+        }
+
+        using (var pipeClient = new NamedPipeClientStream(".", "HealthCarePipe", PipeDirection.InOut))
+        {
+            try
+            {
+                Console.WriteLine("Connecting to server...");
+                pipeClient.Connect();
+
+                SendFile(pipeClient, filePath);
+                Console.WriteLine("File sent to server.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+
+        Console.WriteLine("Press enter to exit.");
+        Console.ReadLine();
+    }
+
+    static void SendFile(NamedPipeClientStream pipeClient, string filePath)
+    {
+        using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+        var buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            pipeClient.Write(buffer, 0, bytesRead);
+        }
+    }
+}
---
+++ NamedPipedTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO.Pipes;
+
+namespace NamedPipedTest.Server;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Healthcare server is running...");
+        StartServerAsync();
+        Console.WriteLine("Press enter to exit");
+        Console.ReadLine();
+    }
+
+    private static async void StartServerAsync()
+    {
+        await Task.Run(StartServer);
+    }
+
+    private static Task StartServer()
+    {
+        while (true)
+        {
+            using var pipeServer = new NamedPipeServerStream("HealthcarePipe", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
+            Console.WriteLine("Waiting for client connection...");
+            pipeServer.WaitForConnection();
+            Console.WriteLine("Client connected!");
+
+            try
+            {
+                ReceiveFile(pipeServer);
+                Console.WriteLine("File received and saved.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+        }
+    }
+
+    static void ReceiveFile(NamedPipeServerStream pipeServer)
+    {
+        var outputFile = "ReceivedFile.dat";
+
+        using var fileStream = new FileStream(outputFile, FileMode.Create, FileAccess.Write);
+        var buffer = new byte[4096];
+        int bytesRead;
+
+        while ((bytesRead = pipeServer.Read(buffer, 0, buffer.Length)) > 0)
+        {
+            fileStream.Write(buffer, 0, bytesRead);
+        }
+    }
+}
